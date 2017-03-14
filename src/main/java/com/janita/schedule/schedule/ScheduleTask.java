986dc10c0ae08eda300a3b78f581f9b429a1c84c
Janita <zhuchenjian@huizhi-tech.com>
2017-03-14 16:37:08 +0800

    计划

--- src/main/java/com/janita/schedule/schedule/ScheduleTask.java
+++ src/main/java/com/janita/schedule/schedule/ScheduleTask.java
@@ -15,6 +15,11 @@ public class ScheduleTask {
 
     @Scheduled(fixedRate = 5000)
     public void schedule(){
-        System.out.println(DateUtil.fmtTime(System.currentTimeMillis()));
+        System.out.println("固定执行频率，fixedRate = 5000 :"+DateUtil.fmtTime(System.currentTimeMillis()));
+    }
+
+//    @Scheduled(cron = "1 0 0 0 0 0")
+    public void taskOne(){
+        System.out.println("每秒执行一次，cron = \"1 0 0 0 0 0\" :"+DateUtil.fmtTime(System.currentTimeMillis()));
     }
 }
