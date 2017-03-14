e2547797bd82be7c5dbd4b724d72c28c7d8523ec
Janita <zhuchenjian@huizhi-tech.com>
2017-03-14 16:14:14 +0800

    Initial commit

---
+++ src/main/java/com/janita/schedule/ScheduleDemoApplication.java
@@ -0,0 +1,12 @@
+package com.janita.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleDemoApplication.class, args);
+	}
+}
